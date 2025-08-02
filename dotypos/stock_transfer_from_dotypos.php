5b5b01f6fb0ad51e61d1570a16496150dc03dee9
Jiří Liška <jiri.liska.95@gmail.com>
2025-08-02 08:45:01 +0200

    Release 2.0.67

--- dotypos/stock_transfer_from_dotypos.php
+++ dotypos/stock_transfer_from_dotypos.php
@@ -1,113 +1,75 @@
 <?php
 
-function stock_transfer_from_dotypos()
-{
-    central_logs("Start", "", "info");
-    $productsToChange = [];
-    $i = 0;
-    $p = 0;
+function stock_transfer_from_dotypos() {
+    
     $access_token_data = dotypos_sync_getDotyposAccessToken();
+    
+ if($access_token_data){   
+$request_url = 'https://api.dotykacka.cz/v2/clouds/'.$access_token_data['cloudid'].'/warehouses/'.$access_token_data['warehouse_id'].'/products?page=1&limit=100&filter=deleted%7Ceq%7Cfalse&sort=name';
 
-    if ($access_token_data) {
+    do {
+        $curl = curl_init();
 
-        $page = 1;
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => $request_url,
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_HTTPHEADER => array(
+            'Accept: application/json; charset=UTF-8',
+            'Content-Type: application/json; charset=UTF-8',
+            'Authorization: Bearer '.$access_token_data["access_token"]
+          ),
+        ));
 
-        do {
+        $response = curl_exec($curl);
 
-            $request_url =
-            "https://api.dotykacka.cz/v2/clouds/" .
-            $access_token_data["cloudid"] .
-            "/warehouses/" .
-            $access_token_data["warehouse_id"] .
-            "/products?page=".$page."&limit=100&filter=deleted%7Ceq%7Cfalse&sort=id";
+        $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        if ($status_code == 404){
+        }
 
-            $curl = curl_init();
-
-            curl_setopt_array($curl, [
-                CURLOPT_URL => $request_url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_HTTPHEADER => [
-                    "Accept: application/json; charset=UTF-8",
-                    "Content-Type: application/json; charset=UTF-8",
-                    "Authorization: Bearer " .
-                    $access_token_data["access_token"],
-                ],
-            ]);
-
-            $response = curl_exec($curl);
-
-            $status_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-            if ($status_code == 404) {
-                continue;
-            }
-
-            if ($status_code == 200) {
-                $data = json_decode($response, true);
-                $current_page = $data["currentPage"];
-                $next_page = $data["nextPage"];
-
-                foreach ($data["data"] as $product) {
-                    if (!empty($product["plu"][0])) {
-                        $sku = $product["plu"][0];
-                        $quantity = $product["stockQuantityStatus"];
-
-                        $productsToChange[] = [
-                            "sku" => $sku,
-                            "quantity" => $quantity
-                        ];
-
-                    }
-
-                    $p++;
-                }
-
-                if (!is_null($next_page)) {
+        if ($status_code == 200) {
+            $data = json_decode($response, true);
+            $current_page = $data["currentPage"];
+            $next_page = $data["nextPage"];
+           
+            foreach ($data["data"] as $product) {
+                
+                if(!empty($product['plu'][0])){
+                    $sku = $product['plu'][0];
+                    $quantity = $product["stockQuantityStatus"];
                     
-                    $page = $next_page;
-                    
-                }
-            } else {
-            }
+           if($woo_product_id = dotypos_sync_get_product_id_by_sku($sku)){
 
-            $i++;
-            central_logs($i . " -> " . $p . 'NextPage -> '.$next_page . ' / CurrentPage -> '.$current_page, "", "info");
-
-        } while (!is_null($next_page));
-
-        curl_close($curl);
-
-        products_stock_update($productsToChange);
-    }
-}
-
-
-function products_stock_update($data){
-
-    if($data){
-
-        foreach($data as $row){
-
-        if($woo_product_id = dotypos_sync_get_product_id_by_sku($row["sku"])){
             
             $woo_products = wc_get_product($woo_product_id);
            //Načte daný produkt
-
-           central_logs('Change -> '.$woo_product_id,'','info');
            
                     // Nastavení správy zásob na true
-                   $woo_products->set_manage_stock(true);
+                    $woo_products->set_manage_stock(true);
                    // Nastaví nové množství na skladě a uloží změny
-                  $woo_products->set_stock_quantity($row["quantity"]);
-                $woo_products->save();
+                    $woo_products->set_stock_quantity($quantity);
+                    $woo_products->save();
+               
+           }
+                }
+
+            }
+
+            if (!is_null($next_page)) {
+                $requestUrl = 'https://api.dotykacka.cz/v2/clouds/'.$cloudid.'/warehouses/'.$warehouse_id.'/products?page='.$next_page.'&limit=100&filter=deleted%7Ceq%7Cfalse';
+                curl_close($curl);
+            }
+}else{
+
+
         }
-    }
-    }
+    } while (!is_null($next_page));
+}
 
 }
